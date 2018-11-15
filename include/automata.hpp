b5ad9700cbbea417411277674400a1279a1b1060
Gorzoid <5949829+Gorzoid@users.noreply.github.com>
2018-11-15 15:08:53 +0000

    First test passes!

--- include/automata.hpp
+++ include/automata.hpp
@@ -1,6 +1,55 @@
-#include <memory>
-#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 
-namespace cellular
+namespace automata
 {
+    class dfsa
+    {
+    public: // temporary public everything so we can test smaller features.
+        class node
+        {
+            std::unordered_map<char, int> m_mapping; // mapping from char to index in dfsa::nodes
+            bool m_finalState;
+        public:
+            node(std::unordered_map<char, int> mapping, bool finalState = false)
+                : m_mapping(std::move(mapping)), m_finalState(finalState)
+            {
+
+            }
+
+            int transition(char c) const
+            {
+                auto it = m_mapping.find(c);
+                if(it == m_mapping.end()) return -1;
+                return it->second;
+            }
+
+            bool finalState() const
+            {
+                return m_finalState;
+            }
+        };
+
+    //private:
+        std::vector<node> nodes;
+    public:
+        dfsa(const std::string& regular_expression);
+        dfsa() = default;
+
+        void addNode(node newNode)
+        {
+            nodes.push_back(std::move(newNode));
+        }
+
+        bool match(const std::string& str)
+        {
+            int i = 0;
+            return std::all_of(str.begin(), str.end(), [this,&i](char c) mutable {
+                i = nodes[i].transition(c);
+                return i >= 0;
+            }) && nodes[i].finalState();
+        }
+    };
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,19 @@
 #include <gtest/gtest.h>
 #include <automata.hpp>
 
-TEST(Foo, Bar)
+TEST(DFSA, SimpleCyclicMatch)
 {
+    using namespace automata;
+    dfsa machine;
+    machine.addNode(dfsa::node{
+        {
+            {'a', 0}, // cyclic
+        },
+        true
+    });
 
+    EXPECT_TRUE(machine.match("a"));
+    EXPECT_TRUE(machine.match("aaaa"));
+    EXPECT_FALSE(machine.match("baaa"));
+    EXPECT_FALSE(machine.match("aaab"));
 }
