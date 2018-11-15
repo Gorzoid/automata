5d47b30f4555af38d35d9593a38752da8880c5d0
Gorzoid <5949829+Gorzoid@users.noreply.github.com>
2018-11-15 22:25:34 +0000

    2nd Test Passing

--- test/test.cpp
+++ test/test.cpp
@@ -17,3 +17,31 @@ TEST(DFSA, SimpleCyclicMatch)
     EXPECT_FALSE(machine.match("baaa"));
     EXPECT_FALSE(machine.match("aaab"));
 }
+
+
+TEST(DFSA, SimpleSequenceMatch)
+{
+    using namespace automata;
+    dfsa machine;
+    machine.addNode(dfsa::node{
+        {
+            {'a', 1},
+        }
+    });
+    machine.addNode(dfsa::node{
+        {
+            {'b', 2},
+        }
+    });
+    machine.addNode(dfsa::node{
+        {
+            {'c', 3},
+        }
+    });
+    machine.addNode(dfsa::node{{}, true});
+
+    EXPECT_TRUE(machine.match("abc"));
+    EXPECT_FALSE(machine.match("aabc"));
+    EXPECT_FALSE(machine.match("abcd"));
+    EXPECT_FALSE(machine.match("abcc"));
+}
